0f9733c2f81437bf0c5593624a88828383570d44
LillyB26 <26442035@sun.ac.za>
2026-01-15 15:04:41 +0200

    initial code for praticing python basics

---
+++ python_basics.py
@@ -0,0 +1,19 @@
+
+#Practice Question for Variables and Data Types
+def question_1():
+    student_name = "Lilly"
+    age = 18
+    marks = ["75", "60", "65", "85", "90"]
+
+    print(f"{student_name} is {age} and got the following grades {marks}")
+
+def question_2():
+    x = "10"
+    y = 5
+    sum = int(x) + y
+    return sum
+
+#Practice questions for conditionals
+
+
+
