e755ae4845c1f7bd51e26110d411f06cd60f598f
LillyB26 <26442035@sun.ac.za>
2026-01-16 16:23:35 +0200

    Practicing functions and logic

--- python_basics.py
+++ python_basics.py
@@ -14,6 +14,74 @@ def question_2():
     return sum
 
 #Practice questions for conditionals
+def pass_or_fail(mark):
+    if mark >= 50:
+        return "pass"
+    else:
+        return "fail"
+    
+def return_pass(marks):
+    pass_marks = []
+    for i in range(len(marks)):
+        if marks[i] >= 50:
+            pass_marks += [marks[i]]
+            
+    return pass_marks
 
 
 
+#Practicing loops
+def even_or_odd(n):
+    i = 1
+    while i <= n:
+        if i % 2 == 0:
+            print(f"{i} is an even number")
+        else:
+            print(f"{i} is an odd number")
+        
+        i += 1
+        
+def pass_or_fail(students):
+    for student , mark in students.items():
+        if mark >= 50:
+            print(f"{student} passed")
+            
+        else:
+            print(f"{student} failed")
+students = {
+    "Lebo" : 78,
+    "Ayesha": 49,
+    "Tom": 65
+}
+
+#Practicing list and dictionaries
+def average(numbers):
+    sum = 0
+    if len(numbers) == 0:
+        return 0
+    else:
+        for i in numbers:
+            sum += i
+            
+        return sum/len(numbers)
+
+        
+
+def sale(sales):
+    profitable_sales = []
+    
+    for i in sales:
+        if i > 900:
+            profitable_sales += [i]
+            
+    return profitable_sales
+
+sales = [1200, 800, 950, 1500, 400]
+print(sale(sales))
+
+#functions and logic
+def highest_mark(marks):
+    
+    return
+
+marks = ["75", "60", "65", "85", "90"]
